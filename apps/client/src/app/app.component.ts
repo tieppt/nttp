b527d4289e72a9f9d229bc2d4ee0d71327f509c4
Tiep Phan <tieppt12@gmail.com>
2018-10-16 06:59:47 +0700

    feat(core): add 3rd UI lib

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -8,6 +8,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'client: ' + environment.production;
+  array = [1, 2, 3, 4];
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1'
+    },
+    {
+      active: false,
+      name: 'This is panel header 2'
+    },
+    {
+      active: false,
+      name: 'This is panel header 3'
+    }
+  ];
+
   constructor(private http: HttpClient) {}
   ngOnInit() {
     this.http.get('https://jsonplaceholder.typicode.com/users/1').subscribe(console.log);
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,11 +1,17 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { CoreModule, TOKEN_ID, TOKEN_TYPE } from '@nttp/web/core';
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
+registerLocaleData(en);
+
 function getTokenId(): string {
   // logic here
   return 'Token_ID';
@@ -14,20 +20,17 @@ function getTokenId(): string {
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NgZorroAntdModule,
     RouterModule.forRoot(APP_ROUTES),
     CoreModule
   ],
   providers: [
-    {
-      provide: TOKEN_TYPE,
-      useValue: 'Bearer'
-    },
-    {
-      provide: TOKEN_ID,
-      useFactory: getTokenId
-    }
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: TOKEN_TYPE, useValue: 'Bearer' },
+    { provide: TOKEN_ID, useFactory: getTokenId }
   ],
   bootstrap: [AppComponent]
 })
